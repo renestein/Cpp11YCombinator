6ef7419b80eafee963123b751d43b270d035296b
Rene Stein <rene@renestein.net>
2014-07-30 13:27:20 +0200

    V2 - anonymous recursion

--- Cpp11YCombinator/Fibonacci.cpp
+++ Cpp11YCombinator/Fibonacci.cpp
@@ -3,29 +3,81 @@
 #include <functional>
 using namespace std;
 
+
+using FirstLevelFunction = std::function < int(int) > ;
+using SecondLevelFunction = std::function < FirstLevelFunction(FirstLevelFunction) > ;
+
+using SecondLevelFirstLevelFunction = std::function <FirstLevelFunction(SecondLevelFunction) > ;
+
+class ThirdLevelFunctionSelf;
+
+using ThirdLevelFunction = std::function < FirstLevelFunction(ThirdLevelFunctionSelf) > ;
+
+class ThirdLevelFunctionSelf
+{
+
+public:
+
+	ThirdLevelFunctionSelf(ThirdLevelFunction function)
+	{
+		m_function = function;
+	}
+
+	FirstLevelFunction operator()(ThirdLevelFunctionSelf &self) const
+	{
+		return m_function(m_function);
+	}
+
+	FirstLevelFunction operator()(const ThirdLevelFunctionSelf &self) const
+	{
+		return m_function(m_function);
+	}
+
+	operator ThirdLevelFunction() const
+	{
+		return m_function;
+	}
+
+private:
+	ThirdLevelFunction m_function;
+};
+
+
 int Fibonacci(int number)
 {
 
-	using PrimaryFunction = std::function < int(int) > ;
 
-	PrimaryFunction fibonacci = [&fibonacci](int number)
+
+	auto fibonacciOuter = [](FirstLevelFunction recursiveFunction) -> FirstLevelFunction
 	{
-		return number == 0 || number == 1 ?
-			1 :
-			fibonacci(number - 1) + fibonacci(number - 2);
+		return [recursiveFunction](int n)
+		{
+			return (n == 0 || n == 1)
+				? 1
+				: recursiveFunction(n - 1) + recursiveFunction(n - 2);
+
+		};
+
 	};
 
-	return fibonacci(number);
+	SecondLevelFirstLevelFunction recursiveFunction = [&recursiveFunction](SecondLevelFunction function2) -> FirstLevelFunction
+	{
+
+		ThirdLevelFunction recursiveInner = [function2](const ThirdLevelFunctionSelf self)->FirstLevelFunction
+		{
+			return[self, function2](int number) -> int
+			{
+				return function2(self(self)) (number);
+			};
+		};
+
+		return recursiveInner(recursiveInner);
+	};
+
+	auto recursiveFibonaccciFunction = recursiveFunction(fibonacciOuter);
+	return recursiveFibonaccciFunction(number);
 };
 
-/*auto fibonacciOuter = [](PrimaryFunction function)
-{
-return[function](int number) {number == 0 || number == 1 ?
-1 :
-function(number - 1) + function(number - 2); };
-};
-
-auto fibonacciInner = fibonacciOuter(fibonacciOuter)*/
 
 
 
